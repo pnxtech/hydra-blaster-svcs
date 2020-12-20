5eeb4aa590462f7764e9a667e333b1e401b99b17
cjus <Carlos.Justiniano@gmail.com>
2020-12-20 09:56:10 -0700

    added missing fonts file and continued wip

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,25 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
 
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
+document.addEventListener('init', (event) => {
+  const navItem = document.querySelectorAll('#menu ons-list-item');
+  navItem.forEach((element)=> {
+    element.addEventListener('click', loadPage);
+  });
+
+  const openNav = document.querySelectorAll('ons-toolbar-button');
+  openNav.forEach((element) => {
+    element.addEventListener('click', openMenu);
+  });
 });
+
+function loadPage(event) {
+  let page = this.getAttribute('href');
+  let content = document.getElementById('content');
+  let menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+}
+
+function openMenu (event) {
+  let menu = document.getElementById('menu');
+  menu.open();
+}
